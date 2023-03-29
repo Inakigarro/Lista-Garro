fde1cb8a5687f0c07d6c2c0cd2daf766f122e1f7
inakigarro <inakigarro@gmail.com>
2023-03-29 07:53:52 -0300

    feat: add student list with buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { alumno } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Lista-Garro';
+  public alumnos: alumno[] = [
+    {
+      nombre: 'Nero Caldwell',
+      email: 'dui.quis.accumsan@hotmail.ca',
+      pais: 'Vietnam',
+      marcado: false,
+    },
+    {
+      nombre: 'Connor Frederick',
+      email: 'donec.fringilla@google.net',
+      pais: 'Belgium',
+      marcado: false,
+    },
+    {
+      nombre: 'Teagan Cherry',
+      email: 'lacus.quisque.purus@hotmail.edu',
+      pais: 'Spain',
+      marcado: false,
+    },
+    {
+      nombre: 'Lacy Thompson',
+      email: 'vel@google.net',
+      pais: 'Spain',
+      marcado: false,
+    },
+    {
+      nombre: 'Meghan Carver',
+      email: 'magnis.dis.parturient@hotmail.com',
+      pais: 'United States',
+      marcado: false,
+    },
+  ];
+
+  public marcarAlumno(index: number) {
+    const alumno = this.alumnos[index];
+    alumno.marcado = !alumno.marcado;
+    console.log(alumno.marcado);
+  }
 }
---
+++ src/app/models/interfaces.ts
@@ -0,0 +1,6 @@
+export interface alumno {
+  nombre: string;
+  email: string;
+  pais: string;
+  marcado: boolean;
+}
